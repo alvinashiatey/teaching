65833d827daa063ed8e3b20d3aafb5bd25a3e980
Alvin Ashiatey <alvinashiatey@gmail.com>
2022-09-02 22:59:54 -0400

    Fix class nav on mobile

--- src/client/ts/home.ts
+++ src/client/ts/home.ts
@@ -1,4 +1,22 @@
-
 export default function log (){
+    console.log(
+        `
+               ,.,
+      MMMM_    ,..,
+        "_ "__"MMMMM          ,...,,
+ ,..., __." --"    ,.,     _-"MMMMMMM
+MMMMMM"___ "_._   MMM"_."" _ """"""
+ """""    "" , \\_.   "_. ."
+        ,., _"__ \\__./ ."
+       MMMMM_"  "_    ./
+        ''''      (    )
+ ._______________.-'____"---._.
+  \\                          /
+   \\________________________/
+   (_)                    (_)
+   
+   This website is a tree ...
+        `
+    )
     console.log("---- Design and developed by Alvin Ashiatey ----")
 }
